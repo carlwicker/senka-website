511a34c38bee0021859d9ccc356645f0c500feef
Carl Wicker <carlwicker@gmail.com>
2025-04-03 16:31:12 +0100

    footer margin fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaInstagram } from "react-icons/fa6";
 export default function Footer() {
   return (
     <div className="bg-[#111]">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-5">
         <div className="text-[#888] font-semibold flex items-center h-60 mt-20 w-full">
           <div className="w-1/2">
             <div>SENKA LOOSEMORE</div>
