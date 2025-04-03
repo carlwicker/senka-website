e861f2dc70541e48bc7763de516f7c9331570679
Carl Wicker <carlwicker@gmail.com>
2025-04-03 16:26:35 +0100

    template completed, awaiting content

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import Header from "@/components/Header";
+import ImageCollection from "@/components/ImageCollection";
 import SubHeader from "@/components/SubHeader";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-white flex flex-col text-black container mx-auto gap-40">
-      <Header />
-      <SubHeader />
+    <div>
+      <div className="flex flex-col gap-20 px-5">
+        <div className="bg-white flex flex-col text-black container mx-auto gap-40">
+          <header className="flex flex-col gap-20">
+            <Header />
+            <SubHeader />
+          </header>
+          <main className="flex flex-col gap-20">
+            <ImageCollection />
+            <ImageCollection />
+            <ImageCollection />
+          </main>
+        </div>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- components/ImageCollection.tsx
+++ components/ImageCollection.tsx
@@ -1,3 +1,39 @@
 export default function ImageCollection() {
-  return <div>ImageCollection</div>;
+  return (
+    <div className="flex gap-20 lg:flex-row flex-col-reverse">
+      <div className="flex flex-col w-full lg:w-1/3 h-fit gap-10">
+        <div>
+          <div className="font-bold">Dark Night of the Soul</div>
+          <div className="font-light">616mm x 1169mm</div>
+          <div className="font-light">2025</div>
+        </div>
+        <p>
+          I was diagnosed with Bipolar Type 1 in 2001, following two psychotic
+          episodes in India. Twelve years later, I experienced two more breaks,
+          all of which resulted in admissions to various psychiatric wards under
+          the Mental Health Act 1982, Section Two.
+        </p>
+        <p>
+          In many cultures, psychosis is viewed not as a mental illness but as a
+          Spiritual Emergency. My own experiences reflected this perspective. I
+          felt as though a veil was being lifted to reveal a deeper reality,
+          allowing me to see, hear, and experience life in a more profound and
+          enlightened way. While this was at times awe-inspiring, it was also
+          accompanied by fear and uncertainty.
+        </p>
+        <p>
+          I've encountered a number of “Dark Nights of the Soul,” some of which
+          felt like an ego death, and others like a re-enactment of past trauma.
+          During the night that I have represented in this piece, I was
+          convinced that my daughter had been killed, that my legs had been
+          broken, and that I was blinded by fear. Through years of reflection, I
+          now view these episodes as both a rapid spiritual journey and a mental
+          health condition in need of treatment. I believe a more comprehensive
+          understanding is needed—one that bridges both the spiritual and mental
+          health aspects, which is currently lacking.
+        </p>
+      </div>
+      <div className="bg-indigo-700 w-full lg:w-2/3 h-300"></div>
+    </div>
+  );
 }
