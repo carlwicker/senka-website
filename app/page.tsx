45782664994a7ef420b47b68e7bca267dc93dced
Carl Wicker <carlwicker@gmail.com>
2025-04-03 13:21:35 +0100

    init titles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="bg-white flex justify-center items-center flex-col h-screen text-black container mx-auto gap-20">
